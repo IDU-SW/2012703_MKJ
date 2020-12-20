36eeaa275c4a9bc03f0d8c3fa6c8aaa98e8787c0
Min KyungJoon <94mkjoon@gmail.com>
2020-12-20 18:38:04 +0900

    Add Data and Complete subject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import movie from './movie.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const items = movie.Item.map((item, index) => {
+      const casts = item.casts.map( (cast) => {
+        return (
+          cast + ","
+        );
+      });
+      return (
+        <ol key={index}>
+          <h3> {index}. {item.name} </h3>
+          <ul>
+            <li> <b>장르</b>: {item.genre} </li>
+            <li> <b>국가</b>: {item.country} </li>
+            <li> <b>개봉일</b>: {item.opened} </li>
+            <li> <b>감독</b>: {item.director} </li>
+            <li> <b>출연진</b>: {casts} </li>
+            <li> <b>등급</b>: {item.grade}세 이상 </li>
+            <li> <b>링크</b>: <a href = {item.link} target = "self" > {item.link}</a> </li>
+          </ul>
+        </ol>
+      );
+    });
+    return (
+      items
+    );
+  }
 }
-
 export default App;
